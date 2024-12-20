27892533bd7c506c071b551e05b4f3677595cdb2
Danniel-Ang <danniel@ui.ac.id>
2024-12-20 23:42:25 +0700

    Gasss

--- lib/auth/screens/register.dart
+++ lib/auth/screens/register.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:steve_mobile/auth/screens/login.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
